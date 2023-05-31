5e9f541b350cc9496a34f4db46620e2a1b324177
Nilo3 <nilo.echezarreta@gmail.com>
2023-05-31 13:19:30 -0300

    final commit

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -83,29 +83,24 @@ const Experience = () => {
   ]
 
   return (
-    <div name="experience" className="bg-gradient-to-b from bg-gray-800  to-black w-full h-screen overflow-auto">
-       <div className='max-w-screen-lg p-4 mx-auto flex flex-col justify-center min-h-screen text-white'>
-        <div >
+    <div name="experience" className="bg-gradient-to-b from bg-gray-800  to-black w-full min-h-screen">
+      <div className='max-w-screen-lg p-4 mx-auto flex flex-col justify-center text-white'>
+        <div>
           <p className="text-4xl font-bold border-b-4 border-gray-500 p-2 inline">Skills</p>
-          <p className="py-6">This are the technologies I've worked with</p>
+          <p className="py-6">These are the technologies I've worked with</p>
         </div>
 
         <div className="w-full grid grid-cols-2 sm:grid-cols-3 gap-8 text-center py-8 px-12 sm:px-0">
-
-            {
-              techs.map(({id,src,title,style}) => (
-                <div key={id} className={`shadow-md hover:scale-105 duration-500 py-2 rounded-lg ${style}`}>
-                <img src={src} alt="" className="w-20 mx-auto" />
-                <p className="mt-4">{title}</p>
-              </div>
-              ))
-            }
-
-          
+          {techs.map(({ id, src, title, style }) => (
+            <div key={id} className={`shadow-md hover:scale-105 duration-500 py-2 rounded-lg ${style}`}>
+              <img src={src} alt="" className="w-20 mx-auto" />
+              <p className="mt-4">{title}</p>
+            </div>
+          ))}
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Experience
+export default Experience;
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -8,7 +8,9 @@ const Home = () => {
       <div name="home" className=' h-screen w-full bg-gradient-to-b from-black via-black to-gray-800 '>
         <div className='max-w-screen-lg mx-auto flex flex-col items-center justify-center h-full px-4 md:flex-row md:justify-between'>
           <div className='flex flex-col justify-center md:w-1/3'>
-            <h2 className='text-4xl sm:text-7xl font-bold text-white'>I'm a Full Stack Developer</h2>
+            <h4 className='text-4xl sm:text-7xl font-bold text-white'>Nilo Echezarreta</h4>
+            <br />
+            <h4 className='text-1xl sm:text-4xl font-bold text-white'>Full Stack Developer</h4>
             <p className='text-gray-500 py-4 max-w-screen-md'>
               Experienced software developer with a year of expertise in web application development using React, Tailwind, and Node.js. Passionate about creating intuitive and responsive user interfaces.
             </p>
