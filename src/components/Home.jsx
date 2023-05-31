bd3f0faee58cc8f836592c00e581e8604993d45e
Nilo3 <nilo.echezarreta@gmail.com>
2023-05-31 16:16:38 -0300

    final commit

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -8,7 +8,7 @@ const Home = () => {
       <div name="home" className=' h-screen w-full bg-gradient-to-b from-black via-black to-gray-800 '>
         <div className='max-w-screen-lg mx-auto flex flex-col items-center justify-center h-full px-4 md:flex-row md:justify-between'>
           <div className='flex flex-col justify-center md:w-1/3'>
-            <h4 className='text-4xl sm:text-7xl font-bold text-white'>Nilo Echezarreta</h4>
+            <h4 className='text-4xl sm:text-6xl font-bold text-white'>Nilo Echezarreta</h4>
             <br />
             <h4 className='text-1xl sm:text-4xl font-bold text-white'>Full Stack Developer</h4>
             <p className='text-gray-500 py-4 max-w-screen-md'>
